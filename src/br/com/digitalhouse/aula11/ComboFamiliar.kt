e3b70ccf082496a04a0360e847e319c0cd21eace
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-09-21 20:44:22 -0300

    Ajustes das clases de combos para aplicar herança

--- src/br/com/digitalhouse/aula11/ComboFamiliar.kt
+++ src/br/com/digitalhouse/aula11/ComboFamiliar.kt
@@ -1,12 +1,10 @@
 package br.com.digitalhouse.aula11
 
-class ComboFamiliar(var combos: MutableList<ComboIndividual>) {
-    var preco = 0.0
-    fun somarPreco() {
-
-    }
-
-    fun aplicarDesconto() {
-
+class ComboFamiliar(var combos: MutableList<ComboIndividual>, override var nome: String, override var desconto: Double) : Combo {
+    override var preco = 0.0
+    override fun somarPreco() {
+        for (combo in combos) {
+            preco += combo.preco
+        }
     }
 }
--- src/br/com/digitalhouse/aula11/ComboIndividual.kt
+++ src/br/com/digitalhouse/aula11/ComboIndividual.kt
@@ -1,16 +1,12 @@
 package br.com.digitalhouse.aula11
 
-class ComboIndividual(var sanduiche: List<Sanduiche>, var acompanhamento: MutableList<Acompanhamento>) {
-    fun somarPreco() {
+class ComboIndividual(var sanduiche: List<Sanduiche>, var acompanhamento: MutableList<Acompanhamento>, override var nome: String, override var preco: Double, override var desconto: Double) : Combo {
+    override fun somarPreco() {
         for (sanduiche in sanduiche) {
-            preco += combo.preco
+            preco += sanduiche.preco
         }
         for (acompanhamento in acompanhamento) {
             preco += acompanhamento.preco;
         }
-    }
-
-    fun aplicarDesconto() {
-
     }
 }
